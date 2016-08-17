b11ea6304e40e34644459ee73afc6b8e4e1dc4e4
rockstar016 <https://github.com/rockstar016/LaurenWork.git>
2016-08-17 14:01:04 +0100

    First screen, Graph doesn't works smoothly

---
+++ app/src/main/java/com/example/ky/bpm/Fragments/MeasureFragment.java
@@ -0,0 +1,342 @@
+package com.example.ky.bpm.Fragments;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.os.PowerManager;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.ky.bpm.ImageProcessing;
+import com.example.ky.bpm.R;
+import com.example.ky.bpm.comps.CircleProgressBar;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class MeasureFragment extends Fragment {
+    private static final String TAG = "HeartRateMonitor";
+    private static final AtomicBoolean processing = new AtomicBoolean(false);
+    private static SurfaceView preview = null;
+    private static SurfaceHolder previewHolder = null;
+    private static Camera camera = null;
+    private TextView text = null;
+    private TextView text_avg_rest = null, text_avg_active = null;
+    private CircleProgressBar progressBar = null;
+    private LineChart chart =null;
+    private static PowerManager.WakeLock wakeLock = null;
+    private static int averageIndex = 0;
+    private static final int averageArraySize = 4;
+    private static final int[] averageArray = new int[averageArraySize];
+    public static enum TYPE {
+        GREEN, RED
+    };
+    private static TYPE currentType = TYPE.GREEN;
+    private static int beatsIndex = 0;
+    private static final int beatsArraySize = 3;
+    private static final int[] beatsArray = new int[beatsArraySize];
+    private static double beats = 0;
+    private static long startTime = 0;
+    private static float current_progress = 0;
+    public MeasureFragment() {}
+    public static MeasureFragment _this;
+    public static MeasureFragment newInstance() {
+        MeasureFragment fragment = new MeasureFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        _this = fragment;
+        return fragment;
+    }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+       View rootview = inflater.inflate(R.layout.fragment_measure, container, false);
+        preview = (SurfaceView) rootview.findViewById(R.id.preview);
+        previewHolder = preview.getHolder();
+        previewHolder.addCallback(surfaceCallback);
+        previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        text = (TextView) rootview.findViewById(R.id.txt_bpm);
+        text_avg_rest = (TextView)rootview.findViewById(R.id.txt_graph_value);
+        text_avg_active = (TextView)rootview.findViewById(R.id.txt_graph_avg);
+        progressBar = (CircleProgressBar)rootview.findViewById(R.id.circle_progress);
+        progressBar.setProgress(0);
+        chart = (LineChart)rootview.findViewById(R.id.line_chart);
+        chart.setDescription("");
+        chart.setViewPortOffsets(0, 0, 0, 0);
+        chart.setNoDataTextDescription("You need to provide data for the chart.");
+        chart.setTouchEnabled(false);chart.setDragEnabled(false);chart.setScaleEnabled(false);
+        chart.setDrawGridBackground(false);
+        chart.setPinchZoom(false);
+        chart.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+        LineData data = new LineData();
+        chart.setData(data);
+        XAxis x = chart.getXAxis();
+        x.setEnabled(false);
+        YAxis y = chart.getAxisLeft();
+        y.setLabelCount(6, false);
+        y.setTextColor(Color.TRANSPARENT);
+        y.setPosition(YAxis.YAxisLabelPosition.INSIDE_CHART);
+        y.setDrawGridLines(false);
+        y.setAxisMaxValue(120);
+        y.setAxisMinValue(-50);
+        y.setAxisLineColor(Color.TRANSPARENT);
+        chart.getAxisRight().setEnabled(false);
+        chart.getLegend().setEnabled(false);
+        PowerManager pm = (PowerManager) getActivity().getSystemService(Context.POWER_SERVICE);
+        wakeLock = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK, "DoNotDimScreen");
+        loadAVGRestActivityToManifest();
+
+        return rootview;
+    }
+    public void setCurrentBeat(int beats){
+        String value_disp = String.format("%03d",beats);
+        text.setText(value_disp);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        try {
+            wakeLock.release();
+            camera.setPreviewCallback(null);
+            camera.stopPreview();
+            camera.release();
+            camera = null;
+            saveAVGRestActivityToManifest();
+        } catch(Exception e){}
+    }
+    public void saveAVGRestActivityToManifest(){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt("avgrest", Integer.valueOf(text.getText().toString()).intValue());
+        editor.putInt("avgactive", 0);
+        editor.commit();
+    }
+    public void loadAVGRestActivityToManifest(){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
+        int value_rest = preferences.getInt("avgrest",0);
+        int value_activity = preferences.getInt("avgactive",0);
+        setAvgRestText(value_rest);
+        setAvgActivityText(value_activity);
+    }
+    public void setAvgActivityText(int value){
+        text_avg_active.setText(String.format("%02d",value));
+    }
+    public void setAvgRestText(int value){
+        text_avg_rest.setText(String.format("%02d",value));
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        try {
+            wakeLock.release();
+            camera.setPreviewCallback(null);
+            camera.stopPreview();
+            camera.release();
+            camera = null;
+            saveAVGRestActivityToManifest();
+        }catch(Exception e){}
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        try {
+            wakeLock.acquire();
+            camera = Camera.open();
+            startTime = System.currentTimeMillis();
+            loadAVGRestActivityToManifest();
+        }catch (Exception e){
+
+        }
+    }
+    private static Camera.PreviewCallback previewCallback = new Camera.PreviewCallback() {
+        @Override
+        public void onPreviewFrame(byte[] data, Camera cam) {
+            if (data == null){
+                current_progress = 0;
+                _this.progressBar.setProgress(current_progress);
+                _this.chart.clearValues();
+                _this.addEntry(30);
+                throw new NullPointerException();
+            }
+            Camera.Size size = cam.getParameters().getPreviewSize();
+            if (size == null) throw new NullPointerException();
+
+            if (!processing.compareAndSet(false, true)) return;
+            int width = size.width;
+            int height = size.height;
+            int imgAvg = ImageProcessing.decodeYUV420SPtoRedAvg(data.clone(), height, width);
+            if (imgAvg == 0 || imgAvg == 255) {
+                current_progress = 0;
+                _this.setCurrentBeat(0);
+                _this.progressBar.setProgress(current_progress);
+                _this.chart.clearValues();
+                _this.addEntry(30);
+                processing.set(false);
+                return;
+            }
+            int averageArrayAvg = 0, averageArrayCnt = 0;
+            for (int i = 0; i < averageArray.length; i++) {
+                if (averageArray[i] > 0) {
+                    averageArrayAvg += averageArray[i];
+                    averageArrayCnt++;
+                }
+            }
+            int rollingAverage = (averageArrayCnt > 0) ? (averageArrayAvg / averageArrayCnt) : 0;
+            TYPE newType = currentType;
+            current_progress += 0.2;
+            if (imgAvg < rollingAverage) {
+                newType = TYPE.RED;
+                if (newType != currentType) {
+                    beats++;///////////////sometimes beats comes to me as zero, that makes progress and graph makes so busy....
+                }
+            } else if (imgAvg > rollingAverage) {
+                newType = TYPE.GREEN;
+            }
+            if (averageIndex == averageArraySize) averageIndex = 0;
+            averageArray[averageIndex] = imgAvg;
+            averageIndex++;
+
+            currentType = newType;
+            if(current_progress >= 100){
+                current_progress = 100;
+            }
+            _this.progressBar.setProgress(current_progress);
+
+            long endTime = System.currentTimeMillis();
+            double totalTimeInSecs = (endTime - startTime) / 1000;////////////////here's pointlll Mr.Jakk
+            if (totalTimeInSecs >= 1) {
+                double bps = (beats / totalTimeInSecs);
+                int dpm = (int) (bps * 60d);//
+                Log.d("test",dpm+"");
+                if (dpm < 30 || dpm > 180) {
+                    startTime = System.currentTimeMillis();
+                    beats = 0;
+                    current_progress = 0;
+                    _this.progressBar.setProgress(current_progress);
+                    _this.setCurrentBeat(0);
+                    _this.chart.clearValues();
+                    _this.addEntry(30);
+                    processing.set(false);
+                    return;
+                }
+                if (beatsIndex == beatsArraySize) beatsIndex = 0;
+                beatsArray[beatsIndex] = dpm;
+                beatsIndex++;
+                int beatsArrayAvg = 0;
+                int beatsArrayCnt = 0;
+                for (int i = 0; i < beatsArray.length; i++) {
+                    if (beatsArray[i] > 0) {
+                        beatsArrayAvg += beatsArray[i];
+                        beatsArrayCnt++;
+                    }
+                }
+                int beatsAvg = (beatsArrayAvg / beatsArrayCnt);
+                if(current_progress >= 100)
+                    current_progress = 100;
+                    _this.setCurrentBeat(beatsAvg);
+                    _this.addEntry(beatsAvg);
+                    startTime = System.currentTimeMillis();
+                    beats = 0;
+            }
+            processing.set(false);
+        }
+    };
+
+    private static SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            try {
+                camera.setPreviewDisplay(previewHolder);
+                camera.setPreviewCallback(previewCallback);
+            } catch (Throwable t) {
+                Log.e(TAG, "PreviewDemo-surfaceCallback - Exception in setPreviewDisplay()", t);
+            }
+        }
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            Camera.Parameters parameters = camera.getParameters();
+            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+            Camera.Size size = getSmallestPreviewSize(width, height, parameters);
+            if (size != null) {
+                parameters.setPreviewSize(size.width, size.height);
+                Log.d(TAG, "Using width=" + size.width + " height=" + size.height);
+            }
+            camera.setParameters(parameters);
+            camera.startPreview();
+        }
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            // Ignore
+        }
+    };
+
+    private static Camera.Size getSmallestPreviewSize(int width, int height, Camera.Parameters parameters) {
+        Camera.Size result = null;
+        for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
+            if (size.width <= width && size.height <= height) {
+                if (result == null) {
+                    result = size;
+                } else {
+                    int resultArea = result.width * result.height;
+                    int newArea = size.width * size.height;
+                    if (newArea < resultArea) result = size;
+                }
+            }
+        }
+        return result;
+    }
+
+    private void addEntry(int value_add) {
+        LineData data = chart.getData();
+        data.setDrawValues(false);
+
+        if (data != null) {
+            ILineDataSet set = data.getDataSetByIndex(0);
+            if (set == null) {
+                set = _this.createSet();
+                data.addDataSet(set);
+            }
+            data.addEntry(new Entry(set.getEntryCount(), value_add), 0);
+            data.notifyDataChanged();
+
+            chart.notifyDataSetChanged();
+            chart.setVisibleXRangeMaximum(15);
+            chart.moveViewToX(data.getEntryCount());
+        }
+    }
+    private LineDataSet createSet() {
+        LineDataSet set = new LineDataSet(null, "Dataset");
+        set.setDrawHorizontalHighlightIndicator(false);
+        set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        set.setDrawCircles(false);
+        set.setLineWidth(2);
+        set.setDrawFilled(true);
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setColor(getResources().getColor(R.color.colorAccent));
+        set.setCircleRadius(4f);
+        set.setValueTextSize(0f);
+        set.setDrawValues(false);
+        set.setFillColor(getResources().getColor(R.color.colorAccent));
+        set.addEntry(new Entry(0,30));
+        return set;
+    }
+}
---
+++ app/src/main/java/com/example/ky/bpm/MainScreenActivity.java
@@ -0,0 +1,106 @@
+package com.example.ky.bpm;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.ky.bpm.Fragments.MeasureFragment;
+
+public class MainScreenActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+//        getSupportActionBar().setIcon(R.drawable.green_icon);
+        setTitleActionBar("Measure");
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        settingFragment(1);
+    }
+    private void settingFragment(int pos){
+        Fragment f = null;
+        String tag="";
+        if (pos == 1)
+        {
+            tag = "listfrag";
+            f = MeasureFragment.newInstance();
+        }
+        if (f != null)
+        {
+            while (getSupportFragmentManager().getBackStackEntryCount() > 0){
+                getSupportFragmentManager().popBackStackImmediate();
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_container, f,tag).commit();
+        }
+    }
+
+    public void setTitleActionBar(String title){
+        getSupportActionBar().setTitle(title);
+    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+
+        } else if (id == R.id.nav_manage) {
+
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
